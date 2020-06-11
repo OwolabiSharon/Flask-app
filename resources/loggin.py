82e2d10afdd25c11e65c55fb5903672d7438d4d7
Omot Owolabi <omotowolabi@Owolabis-MacBook-Pro.local>
2020-06-11 14:18:27 +0100

    hyuio

---
+++ resources/loggin.py
@@ -0,0 +1,21 @@
+from models.regg import UserData
+from flask_restful import Resource, reqparse
+
+class login(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('password',
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
+    parser.add_argument('username',
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
+    def post(self):
+        data = UserReg.parser.parse_args()
+        userr =  UserData.find_by_password(data['password'])
+        if UserData.find_by_username(data['username']):
+            return userr.json()
+        return {'message': 'i think you should register before you loggin'}
